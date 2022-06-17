3aa4343db206837efd266e59138b44dc7396ff71
mwikalikyalo <winniemwikali07@gmail.com>
2022-06-17 22:46:21 +0300

    Add the html structure of the landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import image from 'business.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className= "container-fluid">
+  <div className="row">
+  <div className="col-6">
+  <h2>Get <span className='blue'>insights</span>  that help your business grow.</h2>
+  <p className="text-center"> Discover the benefits of data analytics and make better decisions 
+  regarding revenue, customer experience and overall efficiency.</p>
+  </div>
+  <ul className='text-start'>
+    <li className="list-inline-item"><span className='h5'>10K+ </span> COMPANIES</li>
+    <li className="list-inline-item"><span className='h5'>314 </span> TEMPLATES</li>
+    <li className="list-inline-item"><span className='h5'>12M+ </span> QUERIES</li>
+  </ul>
+  </div>
+  <div className="img-fluid">
+  <img src={image} alt="people" />
+  
+  </div>                                    
+  </div>    
     </div>
   );
 }
